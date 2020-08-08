68533f7503ae5faba86f2dc103c443f5cdf3105c
alexdziarn <68079075+alexdziarn@users.noreply.github.com>
2020-08-08 08:21:21 -0400

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,5 +1,5 @@
 <?php
-	$link = mysqli_connect(DATABASE, DB_USERNAME, DB_PASSWORD);
+	$link = mysqli_connect(secrets.DATABASE, secrets.DB_USERNAME, secrets.DB_PASSWORD);
 	if(mysqli_connect_error()){
 			die("Connection failed: ".mysqli_connect_error());
 	}
